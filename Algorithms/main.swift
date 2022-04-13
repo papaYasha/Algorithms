8aa5128e74845172013cce72b4ea9cc65e87787c
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-04-13 20:07:41 +0300

    First ex

--- Algorithms/main.swift
+++ Algorithms/main.swift
@@ -7,5 +7,22 @@
 
 import Foundation
 
-print("Hello, World!")
+// Задача 1.
+// Написать функцию, которая принимает строку как своей единственный параметр и возвращает true, если строка содержит только уникальные символы.
+
+func checkUniqueString(input: String) -> Bool {
+    var checkedSymbols = [Character]()
+    for symbol in input {
+        if checkedSymbols.contains(symbol) {
+            return false
+        }
+        checkedSymbols.append(symbol)
+    }
+    return true
+}
+
+func checkUniqueStringSet(input: String) -> Bool {
+    return Set(input).count == input.count
+}
+
 
